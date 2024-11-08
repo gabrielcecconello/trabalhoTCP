ff05c46db74340186f74b4bf0af9e20c7c49e5a5
paulatalim <paulactalim@gmail.com>
2024-11-08 11:45:03 -0300

    create a timer

---
+++ timer.h
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/errno.h>
+#include <sys/time.h>
+#include <signal.h>
+
+int time;
+
+/**
+ Reinicia o time
+ */
+void set_timer(int miliseconds) {
+    struct itimerval newvalue, oldvalue;
+
+    newvalue.it_value.tv_sec  = miliseconds / 1000;
+    newvalue.it_value.tv_usec = miliseconds % 1000 * 1000;
+    newvalue.it_interval.tv_sec  = 0;
+    newvalue.it_interval.tv_usec = 0;
+    setitimer(ITIMER_REAL, &newvalue, &oldvalue);
+}
+
+void timer_handler(int signum) {
+    /* Uma outra opcao seria setar uma variavel global
+     * e testa-la no loop principal do programa, mas isso
+     * so funcionaria nos casos onde a chamada bloqueada 
+     * fosse interrompida pelo sinal
+     * (o que nao acontece no caso do teclado, pelo menos).
+     */
+    set_timer(time);  /* Melhor lugar para reiniciar o timer */
+}
+
+/*
+ Configura um sinal para o programa alterar seu fluxo apos uma entrada
+*/
+void set_handler(void) {
+    signal(SIGALRM, timer_handler);
+}
+
+/*
+    Configura e inicializa o temporizador
+*/
+void start_timer(int argc, char* argv[]) {
+    size_t elen;
+
+    time = (argc == 2) ? atoi(argv[1]) : 1000;
+
+    set_handler();
+    set_timer(time);
+}
+
+// TODO: avaliar a necessidade dessa funcao
+void update_timer() {
+    char input[512];
+    bool done = false;
+    
+    fprintf(stderr,"Escreva algo:\n");
+
+    // Leitura da entrada colocada
+    if (fgets(input, sizeof(input), stdin) != NULL) {
+        done = true;
+        //break;
+    }
+    
+    errno = 0; /* So por garantia */
+    
+    /* fgets retorna erro se a input estava vazia ou se deu erro */
+    /* chamadas de socket so retornam < 0 se deu erro */
+    if (errno == EINTR) {
+        /* uma chamada interrompida seria tratada aqui */
+        errno = 0;
+    } else if (errno) {
+        perror("fgets");
+        exit(1);
+    } else if (feof(stdin)) {
+        fprintf(stderr,"entrada vazia.\n");
+        exit(0);
+    }
+}
