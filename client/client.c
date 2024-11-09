e78703ad510cd5cc5565360516d9bd31f20dab33
paulatalim <paulactalim@gmail.com>
2024-11-09 19:59:47 -0300

    implement ID client

--- client/client.c
+++ client/client.c
@@ -18,8 +18,23 @@ int main(int argc, char const *argv[]) {
     bool is_sending_mensage = true;
     bool is_fisrt_mensage = true;
 
+    // Verificar se foi fornecido um identificador de cliente (argumento da linha de comando)
+    if (argc != 2) {
+        printf("Uso: %s <ID do Cliente (1001-1999)>\n", argv[0]);
+        return -1;
+    }
+
+    // Usar atoi para converter o ID do cliente (agora funciona corretamente)
+    unsigned short int client_id = atoi(argv[1]);
+
+    // Verificar se o ID é válido (1-999)
+    if (client_id < 1001 || client_id > 1999) {
+        printf("Error: ID do cliente inválido. O ID deve ser entre 1001 e 1999.\n");
+        return -1;
+    }
+
     system("clear");
-    printf("\t\t\t*** Cliente {id} ***\n");
+    printf("\t\t\t*** Cliente %d ***\n", client_id);
 
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
@@ -42,7 +57,7 @@ int main(int argc, char const *argv[]) {
         return -1;
     }
 
-    fill_msg(&mensage, 0, 0, 0, "OI");
+    fill_msg(&mensage, 0, client_id, 0, "OI");
     send_msg(client_fd, &mensage);
     printf("Conectando ao servidor ... \n\n");
 
@@ -51,7 +66,7 @@ int main(int argc, char const *argv[]) {
 
     while(is_sending_mensage) {
         if(!is_fisrt_mensage) {
-            printf("\t\t\t*** Cliente {id} ***\n\n");
+            printf("\t\t\t*** Cliente %d ***\n\n", client_id);
         }
 
         printf("Insira uma mensagem ou [q] para parar\n\n");
@@ -69,7 +84,7 @@ int main(int argc, char const *argv[]) {
             scanf("%d", &mensage_destination);
             fflush(stdout);
 
-            fill_msg(&mensage, 2, 0, mensage_destination, mensage_str);
+            fill_msg(&mensage, 2, client_id, mensage_destination, mensage_str);
             send_msg(client_fd, &mensage);
         } else {
             is_sending_mensage = false;
@@ -79,7 +94,7 @@ int main(int argc, char const *argv[]) {
         system("clear");
     }
 
-    printf("\t\t\t*** Cliente {id} ***\n\nDesligando sistema ...\n\n");
+    printf("\t\t\t*** Cliente %d ***\n\nDesligando sistema ...\n\n", client_id);
     fill_msg(&mensage, 1, 0, 0, "TCHAU");
     send_msg(client_fd, &mensage);
 
